9695a48535cf8dc57fd3d29ee6d19904d2a603e1
destx0 <destisdbest@gmail.com>
2023-03-21 12:41:38 +0530

    first commit

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+import { HimenuAlt4 } from "react-icons/hi";
+import {AiOutlineClose} from "react-icons/ai";
+import logo
+const Navbar = () => {
+  return <h1>Navbar</h1>;
+};
+
+export default Navbar;
